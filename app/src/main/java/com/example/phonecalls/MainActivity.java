4e7c3e3c0ea8a8126f720b9cc84bbdb50dda121e
johnnkp <22496821+johnnkp@users.noreply.github.com>
2019-01-27 20:10:30 +0800

    add RequestPermissions() for CALL_PHONE
    https://developer.android.com/training/permissions/requesting#java

--- app/src/main/java/com/example/phonecalls/MainActivity.java
+++ app/src/main/java/com/example/phonecalls/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.phonecalls;
 import android.support.v7.app.AppCompatActivity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -24,11 +26,19 @@ public class MainActivity extends AppCompatActivity {
         });
     } /* * --------------------------------- * Method: Making Phone Calls * --------------------------------- */
 
+    private void RequestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 0);
+        }
+    }
+
     private void makeCall() {
         Log.i("Make a call", "");
         Intent phoneIntent = new Intent(Intent.ACTION_CALL);
-        phoneIntent.setData(Uri.parse("Tel:25 123 123"));
+        phoneIntent.setData(Uri.parse("tel:25-123-123"));
         try {
+            RequestPermissions();
             startActivity(phoneIntent);
             finish();
             Log.i("Finish making a call...", "");
